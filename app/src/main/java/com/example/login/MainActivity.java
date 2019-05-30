896c374f6ffdc9978169934cdb82b84c0027efaf
Zml99 <malinares99@gmail.com>
2019-05-30 00:45:21 -0400

    Add Login Clear Button

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -5,13 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button clear;
+    private EditText name;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clear = (Button)findViewById(R.id.btnclear);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = (EditText)findViewById(R.id.txtuser);
+                password = (EditText)findViewById(R.id.txtpass);
+
+                name.getText().clear();
+                password.getText().clear();
+            }
+        });
 
 
     }
