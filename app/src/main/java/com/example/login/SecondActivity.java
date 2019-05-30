3ade94224658d8b1f95a144b38a46c8dd521c9e7
adissonfeliz <adissongeremias@gmail.com>
2019-05-30 17:00:55 -0400

    Imagenes-ListView

--- app/src/main/java/com/example/login/SecondActivity.java
+++ app/src/main/java/com/example/login/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,9 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecondActivity extends AppCompatActivity {
 
     private ListView lv1;
@@ -18,14 +22,36 @@ public class SecondActivity extends AppCompatActivity {
 
     public static String  selection;
 
+    private int estudiantes;
+
+    public class  estudiantes extends Activity {
+                private List<estudiantes>  myestudiantes= new ArrayList<estudiantes>();
+
+                @Override
+    protected void onCreate(Bundle saveInstanceState){
+                    super.onCreate(savedInstanceState);
+                    setContentView(R.layout.activity_second);
+
+                    FotoEstudiantes();
+                    FotoEstudiantesView();
+                }
+}
+            private void FotoEstudiantes(){
+
+               adapter.add(new estudiantes("Miguel Ant. Linares S", R.drawable.foto_1));
+               adapter.add(new estudiantes("Antony Hernandez", R.drawable.foto_2));
+               adapter.add(new estudiantes("Jose Carlos Ramirez", R.drawable.foto_3));
+               adapter.add(new estudiantes("Eduardo Herrera", R.drawable.foto_4));
+            }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         lv1 = (ListView)findViewById(R.id.lv1);
-        
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_names, nombres);
+
+        final ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_names, nombres);
         lv1.setAdapter(adapter);
 
         lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -36,6 +62,6 @@ public class SecondActivity extends AppCompatActivity {
                 selection = (String) parent.getItemAtPosition(position);
 
             }
-        });
+            });
     }
 }
